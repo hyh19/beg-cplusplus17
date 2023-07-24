314a13506dd664033b7e97cbcfed25cf1e9099cb
e7e26e5 <abc@xyz.com>
2023-07-24 20:25:15 +0800

    reformat

--- Examples/Chapter14/Ex14_04/ToughPack.h
+++ Examples/Chapter14/Ex14_04/ToughPack.h
@@ -3,17 +3,16 @@
 
 #include "Box.h"
 
-class ToughPack : public Box
-{
+class ToughPack : public Box {
 public:
-  // Constructor
-  ToughPack(double lv, double wv, double hv) : Box {lv, wv, hv} {}
+    // Constructor
+    ToughPack(double lv, double wv, double hv) : Box{lv, wv, hv} {}
 
-  // Function to calculate volume of a ToughPack allowing 15% for packing
-  double volume(int i = 500) const override 
-  {
-    std::cout << "ToughPack parameter = " << i << std::endl;
-    return 0.85 * length*width*height;
-  }
+    // Function to calculate volume of a ToughPack allowing 15% for packing
+    double volume(int i = 500) const override {
+        std::cout << "ToughPack parameter = " << i << std::endl;
+        return 0.85 * length * width * height;
+    }
 };
+
 #endif
