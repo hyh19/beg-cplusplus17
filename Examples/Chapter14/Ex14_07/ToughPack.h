01dda9025a1deda6c9d4b4db79a5543deddbc7b8
e7e26e5 <abc@xyz.com>
2023-07-24 18:58:50 +0800

    reformat

--- Examples/Chapter14/Ex14_07/ToughPack.h
+++ Examples/Chapter14/Ex14_07/ToughPack.h
@@ -3,16 +3,16 @@
 
 #include "Box.h"
 
-class ToughPack : public Box
-{
+class ToughPack : public Box {
 public:
-  // Constructor
-  ToughPack(double lv, double wv, double hv) : Box {lv, wv, hv} {}
-  
-  ~ToughPack() { std::cout << "ToughPack destructor called" << std::endl; }
+    // Constructor
+    ToughPack(double lv, double wv, double hv) : Box{lv, wv, hv} {}
+
+    ~ToughPack() { std::cout << "ToughPack destructor called" << std::endl; }
 
 protected:
-  // Function to calculate volume of a ToughPack allowing 15% for packing
-  double volume() const override { return 0.85 * length*width*height; }
+    // Function to calculate volume of a ToughPack allowing 15% for packing
+    double volume() const override { return 0.85 * length * width * height; }
 };
+
 #endif
