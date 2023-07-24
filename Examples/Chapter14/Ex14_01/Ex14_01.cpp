c9b1266133f5ff301ffe0de1c2c896f540070232
e7e26e5 <abc@xyz.com>
2023-07-24 19:18:36 +0800

    reformat

--- Examples/Chapter14/Ex14_01/Ex14_01.cpp
+++ Examples/Chapter14/Ex14_01/Ex14_01.cpp
@@ -2,15 +2,14 @@
 #include "Box.h"                                // For the Box class
 #include "ToughPack.h"                          // For the ToughPack class
 
-int main()
-{
-  Box box {20.0, 30.0, 40.0};                   // Define a box
-  ToughPack hardcase {20.0, 30.0, 40.0};        // Declare tough box - same size
+int main() {
+    Box box{20.0, 30.0, 40.0};                   // Define a box
+    ToughPack hardcase{20.0, 30.0, 40.0};        // Declare tough box - same size
 
-  box.showVolume();                             // Display volume of base box
-  hardcase.showVolume();                        // Display volume of derived box
+    box.showVolume();                             // Display volume of base box
+    hardcase.showVolume();                        // Display volume of derived box
 
-  //std::cout << "hardcase volume is " << hardcase.volume() << std::endl;
-  //Box *pBox {&hardcase};
-  //std::cout << "hardcase volume through pBox is " << pBox->volume() << std::endl;
+    //std::cout << "hardcase volume is " << hardcase.volume() << std::endl;
+    //Box *pBox {&hardcase};
+    //std::cout << "hardcase volume through pBox is " << pBox->volume() << std::endl;
 }
--- Examples/Chapter14/Ex14_01/ToughPack.h
+++ Examples/Chapter14/Ex14_01/ToughPack.h
@@ -3,13 +3,13 @@
 
 #include "Box.h"
 
-class ToughPack : public Box
-{
+class ToughPack : public Box {
 public:
-  // Constructor
-  ToughPack(double lv, double wv, double hv) : Box {lv, wv, hv} {}
+    // Constructor
+    ToughPack(double lv, double wv, double hv) : Box{lv, wv, hv} {}
 
-  // Function to calculate volume of a ToughPack allowing 15% for packing
-  double volume() const { return 0.85 * length*width*height; }
+    // Function to calculate volume of a ToughPack allowing 15% for packing
+    double volume() const { return 0.85 * length * width * height; }
 };
+
 #endif
